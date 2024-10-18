fec6bbff3e650061f422d8f69092a4dbe58d77f8
Nik Rajcevich <nik@rajcevich.com>
2024-10-18 00:24:17 -0600

    refactor: Clean up unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,20 +11,3 @@ export type ProductRecord = {
 export type ListProductsResponse = {
   products: ProductRecord[];
 };
-
-export type CartItem = Pick<
-  ProductRecord,
-  "id" | "name" | "price" | "image" | "slug"
-> & {
-  quantity: number;
-};
-
-export type CartService = {
-  cartItems: CartItem[];
-  addToCart: (item: Omit<CartItem, "quantity">) => void;
-  removeFromCart: (id: string) => void;
-  updateQuantity: (id: string, quantity: number) => void;
-  clearCart: () => void;
-  totalPrice: () => number;
-  totalItems: () => number;
-};
